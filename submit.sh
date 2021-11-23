b04c0333da90eab7b5adae1fa05c2da1c78945aa
ccat3z <fzhang.chn@foxmail.com>
2021-11-23 18:33:32 +0800

    competition tools: add submit

---
+++ submit.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+set -e
+. "$(dirname "$0")/.env"
+
+cd "${REPO_DIR}"
+"${TOOL_DIR}/build.sh" --no-setup
+
+# shellcheck disable=2087
+ssh -t "$TEST_SERVER" <<EOF
+cd "${REPO_IN_TEST_SERVER}" || exit 1
+rm -rf submit
+mkdir submit
+
+echo -e "\033[34mstrip...\033[0m"
+strip build_release/src/observer/observer -o submit/observer || exit 1
+
+echo -e "\033[34mgzip...\033[0m"
+gzip submit/observer || exit 1
+
+echo -e "\033[34msubmit...\033[0m"
+scp submit/observer.gz ccat3z@172.16.0.232:~/observer.gz
+
+echo -e "\033[34mdone\033[0m"
+EOF
