9f8c137b65a83ddbed291d6613319a517ff95bd7
ccat3z <fzhang.chn@foxmail.com>
2021-11-24 21:37:42 +0800

    competiton tools: add flame

---
+++ flame.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+set -e
+. "$(dirname "$0")/.env"
+
+run_in_server
+
+PID=$(pgrep observer)
+if [ ${#PID} -eq 0 ]
+then
+    echo "observer not running"
+    exit 1
+fi
+
+CACHE_DIR=$HOME/.cache/oceanbase-perf
+[ -d "$CACHE_DIR" ] || mkdir -p "$CACHE_DIR"
+cd "$CACHE_DIR"
+
+RECORD_DURATION=${1:-5}
+
+set -x
+perf record -F 99 --call-graph fp -p "$PID" -o perf.data -- sleep "$RECORD_DURATION"
+perf script -i perf.data > perf.unfold
+"$TOOL_DIR/stackcollapse-perf.pl" perf.unfold > perf.folded
+"$TOOL_DIR/flamegraph.pl" perf.folded
