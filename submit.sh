d2ec51f551f8bd218d25206c2687ea8542c2322a
ccat3z <fzhang.chn@foxmail.com>
2021-12-02 21:06:58 +0800

    competition tools: submit: save key in script

--- submit.sh
+++ submit.sh
@@ -3,6 +3,20 @@
 set -e
 . "$(dirname "$0")/.env"
 
+SUBMIT_KEY="-----BEGIN PGP MESSAGE-----
+
+hQEMA9Xz9e7smekIAQf9HzSz+sl5KnFkIo7cPqPSt6ljgXPY6Ztgrpo/V7d29m2V
+XIaNpXp2ZB40CUDOVQFCk16mojVz/A5k1RUPnTzZhFsUWCJR/kvij4U+5QwbAf6z
+8iMVZmrIq3+VAadddj0sViTRh+Pp7pSPv4kDvPJYtZFSRclgdJt3c8jrODACvkUy
+Kcma+lWBEknfsnyDoXtd4TcklGUfOc5YfmQXyR77h7Iui46SiQrzSHQ0awxkL6ip
+E66iNEtm1jCO6DaeUTORtdS5S3UWc+w8/28avUzYaL01U36kIsqbuTuWH2AdbhHd
+RJUxFn6pa3xJGkLTsbDTuYlnGuqsA7fZ+hPIWr8B49JPAZlir79qBgdRmED2r2Sx
+aY4ysaYKoMnA6tl08pDdPxJLZ2UpXHWHC+8mrMrpYITdhJzKuvk9bJpbnqqu9TnB
+xzNFmawCnBOlf8TkUIjurg==
+=zgGY
+-----END PGP MESSAGE-----"
+SUBMIT_KEY="$(echo "$SUBMIT_KEY" | gpg -d)"
+
 cd "${REPO_DIR}"
 "${TOOL_DIR}/build.sh" --no-setup
 
@@ -19,7 +33,7 @@ echo -e "\033[34mgzip...\033[0m"
 gzip submit/observer || exit 1
 
 echo -e "\033[34msubmit...\033[0m"
-scp submit/observer.gz ccat3z@172.16.0.232:~/observer.gz
+sshpass -p "$SUBMIT_KEY" scp submit/observer.gz ccat3z@172.16.0.232:~/observer.gz
 
 echo -e "\033[34mdone\033[0m"
 EOF
