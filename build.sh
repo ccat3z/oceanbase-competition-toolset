116cb31db77d5c9cde6e86281a656faab990739b
ccat3z <fzhang.chn@foxmail.com>
2021-12-02 20:57:51 +0800

    competition tools: build: new flag: --redeploy

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ set -e
 NEED_SYNC=y
 NEED_INIT=n
 NEED_SETUP=y
+RESTART_METHOD=restart
 while [ -n "$1" ]; do
     case "$1" in
         --no-sync) NEED_SYNC=n ;;
         --init) NEED_INIT=y ;;
         --no-setup) NEED_SETUP=n ;;
+        --redeploy) RESTART_METHOD=redeploy ;;
         --help) log_i "$0 [--no-sync] [--init] [--no-setup] [--help]"; exit 0 ;;
         *) log_e "Unknown arg: $1"; exit 1;
     esac
@@ -31,7 +33,7 @@ ssh -t "$TEST_SERVER" sh -e << EOF
         echo "Setup..."
         obd cluster stop "$OB_CLUSTER_NAME"
         cp \$PWD/build_release/src/observer/observer "$OB_CLUSTER_SERVER_BINARY_PATH"
-        obd cluster restart "$OB_CLUSTER_NAME"
+        obd cluster $RESTART_METHOD "$OB_CLUSTER_NAME"
         obd cluster tenant create ob-benchmark --tenant-name test || true
     fi
 EOF
