e29b7d3bb0fa7eeeee2380e359b3622c77fd2d82
ccat3z <fzhang.chn@foxmail.com>
2022-01-11 15:48:20 +0800

    client: add help

--- client.sh
+++ client.sh
@@ -3,5 +3,10 @@
 set -e
 . "$(dirname "$0")/init.sh"
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "$0 [tenant]" >&2
+    exit 0
+fi
+
 ssh -t "$TEST_CLIENT" \
     "$OB_CLIENT" -c -uroot@${1:-test} -h"$TEST_SERVER_HOST" -P 2188 test
