175f7e8b29114901438395e121c742e51c1a6c3c
ccat3z <fzhang.chn@foxmail.com>
2022-01-11 15:46:48 +0800

    client: support custom tenant

--- client.sh
+++ client.sh
@@ -4,4 +4,4 @@ set -e
 . "$(dirname "$0")/init.sh"
 
 ssh -t "$TEST_CLIENT" \
-    "$OB_CLIENT" -c -uroot@test -h"$TEST_SERVER_HOST" -P 2188 test "$@"
+    "$OB_CLIENT" -c -uroot@${1:-test} -h"$TEST_SERVER_HOST" -P 2188 test
